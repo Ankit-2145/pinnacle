0db7c8c1a475beed88bb1df065704536bf3973a9
Ankit Sharma <ankitsharma2615@gmail.com>
2026-04-17 22:10:10 +0530

    bug fix route /

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/src/pages/learning.html",
-        destination: "/*",
+        destination: "/",
         permanent: true,
       },
     ];
